76ce280bc45f9b563efb5d1474f0b21092f5e305
jamescalderon <j.e.calderon@gmail.com>
2018-01-18 11:37:11 -0500

    Updates

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.css' //BS4
-import './index.css'; //Local CSS
-import App from './App'; //main component
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
